819d7764b0f7fbb13a90ec8ec41248f801084287
theweakestlink <silph021@gmail.com>
2015-07-21 08:13:23 -0400

    Added theme

---
+++ app/src/main/java/com/cosyde/theweakestlink/studentwalkthrough/DatabaseHelper.java
@@ -0,0 +1,34 @@
+package com.cosyde.theweakestlink.studentwalkthrough;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by mobile1 on 7/21/15.
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static DatabaseHelper mInstance = null;
+
+    private DatabaseHelper(Context mContext) {
+        super(mContext, "onex_db", null, 5);
+    }
+
+    public static DatabaseHelper getInstance(Context mContext) {
+        if (mInstance == null) {
+            mInstance = new DatabaseHelper(mContext);
+        }
+        return mInstance;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
--- app/src/main/java/com/cosyde/theweakestlink/studentwalkthrough/Fingertips.java
+++ app/src/main/java/com/cosyde/theweakestlink/studentwalkthrough/Fingertips.java
@@ -2,16 +2,23 @@ package com.cosyde.theweakestlink.studentwalkthrough;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class Fingertips extends ActionBarActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fingertips);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
