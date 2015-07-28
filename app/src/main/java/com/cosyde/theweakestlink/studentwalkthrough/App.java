24031885a9e92fff997db736effd1806ea43b2bc
theweakestlink <silph021@gmail.com>
2015-07-28 04:05:22 -0400

    adde app.java

---
+++ app/src/main/java/com/cosyde/theweakestlink/studentwalkthrough/App.java
@@ -0,0 +1,50 @@
+package com.cosyde.theweakestlink.studentwalkthrough;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+/**
+ * Created by mobile1 on 7/26/15.
+ */
+public class App extends Application {
+
+    // APPLICATION_CONSTANTS
+    public final String TAG = "My application tag";
+
+    private static Context mContext;
+    private static SharedPreferences sharedPreferences;
+    private static SharedPreferences.Editor spEditor;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mContext = getApplicationContext();
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        spEditor = sharedPreferences.edit();
+    }
+
+    public void toast(String message) {
+        // pantamad lang to hehe
+        Toast.makeText(mContext, message, Toast.LENGTH_LONG).show();
+    }
+
+    public static boolean isOnline() {
+        // returns true if user is connected to the internet
+        ConnectivityManager connectivityManager = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkinfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkinfo != null && activeNetworkinfo.isConnected();
+    }
+
+    public void cameraIsAvailable() {
+
+    }
+
+}
+
+
